35c9a446f16b860515896f93a26667e9d1e0ebaa
williamc443 <57712080+williamc443@users.noreply.github.com>
2021-04-23 13:03:57 +0200

    2.nd branch

--- js/bookdetail.js
+++ js/bookdetail.js
@@ -1,59 +1,65 @@
-import { books } from "./script.js";
+import { booksArrey } from "./script.js";
 
 const bookDetails = document.querySelector(".book-detail");
-
 
 
 console.log(bookDetails);
 
 bookDetails.innerHTML = "";
 
-for (let i = 0; i < books.length; i++) {
+for (let i = 0; i < booksArrey.length; i++) {
 
 bookDetails.innerHTML += `<div class="book">
-                                    <h4>Name: ${books[i].title}</h4>
-                                    <p>ISBN: ${books[i].isbn}</p>
-                                    <button data-book="${books[i]}">Remove</buttun>
+                                    <h4>Name: ${booksArrey[i].title}</h4>
+                                    <p id="isbn">ISBN: ${booksArrey[i].isbn}</p>
+                                    <button onclick="remove()">Remove</buttun>
                             </div>`;
+                   }
 
-}
+onclick = remove();
+
+document.getElementById("isbn").innerHTML = bookDetails;
+
+             function remove() {
+                bookDetails.splice(-1);
+               document.getElementById("isbn").innerHTML = bookDetails;
+             }
 
 const removeBookButton = document.querySelectorAll("button");
 
-removeBookButton.forEach(function (books) {
-        books.addEventListener("click", handleClick);
-    });
+removeBookButton.forEach(function (removeBookButton) {
+    removeBookButton.addEventListener("click", handleClick);
+
+    bookDetails.innerHTML = "";
+
+});
+
+    // function handleClick(event) {
+    //     console.log(event);
+
+    //     let booksArray;
+
+    //     const removeItem = (arr, item) => {
+    //         let newArray = [...arr]
+    //         const index = newArray.findIndex((element) => element === item)
+    //         if(index !== -1) {
+    //             newArrry.spice(index, 1)
+    //             return newArray
+
+    //         }
+    //     }
 
 
 
-function handleClick(event) {
-    event.target.classList.toggle("clickevent");
-console.log(event);
-}
+// function removeFromList(event) {
+//     console.log(event);
 
-    // function removeFromList() {
+//     let booksArray;
 
-    //     const removeButton = document.querySelectorAll("button")
-    
-    //     removeButton.forEach(function(trash) {
-    //         trash.addEventListener("click", removeFromList);
-    //     });
-    // }
+//     let remove = `${booksArray[i].isbn}`;
 
+//     booksArrey.splice(booksArrey.indexOf(remove), 1);
+//     console.log(`Array with "${remove}" removed:`, booksArrey);
+// };
 
-function removeFromList(event) {
-    console.log(event);
-
-    const deleteThisBook = event.target.dataset.book;
-
-    const newList = listBook.filter(function (book) {
-        if (deleteThisBook !== book) {
-            return true;
-        }
-    });
-
-    listBooks = newList;
-
-}
-
-
+// removeFromList();
